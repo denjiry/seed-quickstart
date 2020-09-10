578c7914dd97178b950295afb1731c9b6c9833f0
kento tajiri <k.denjiry@gmail.com>
2020-09-11 03:00:39 +0900

    implement view

--- src/lib.rs
+++ src/lib.rs
@@ -146,8 +146,98 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
 // ------ ------
 //     View
 // ------ ------
-fn view(model: &Model) -> Node<Msg> {
-    div!["I'm a placeholder"]
+fn view(model: &Model) -> Vec<Node<Msg>> {
+    nodes![
+        view_header(),
+        IF!(not(model.todos.is_empty()) => vec![
+            view_main(),
+            view_footer(),
+        ]),
+    ]
+}
+
+// ------ header ------
+
+fn view_header() -> Node<Msg> {
+    header![
+        C!["header"],
+        h1!["todos"],
+        input![
+            C!["new-todo"],
+            attrs! {At::Placeholder => "What needs to be done?", At::AutoFocus => AtValue::None},
+        ]
+    ]
+}
+
+// ------ main ------
+
+fn view_main() -> Node<Msg> {
+    section![C!["main"], view_toggle_all(), view_todo_list(),]
+}
+
+fn view_toggle_all() -> Vec<Node<Msg>> {
+    vec![
+        input![
+            C!["toggle-all"],
+            attrs! {At::Id => "toggle-all", At::Type => "checkbox"}
+        ],
+        label![attrs! {At::For => "toggle-all"}, "Mark all as complete"],
+    ]
+}
+
+fn view_todo_list() -> Node<Msg> {
+    ul![
+        C!["todo-list"],
+        // These are here just to show the structure of the list items
+        // List items should get the class `editing` when editing and `completed` when marked as completed
+        li![
+            C!["completed"],
+            div![
+                C!["view"],
+                input![
+                    C!["toggle"],
+                    attrs! {At::Type => "checkbox", At::Checked => AtValue::None}
+                ],
+                label!["Taste JavaScript"],
+                button![C!["destroy"]],
+            ],
+            input![
+                C!["edit"],
+                attrs! {At::Value => "Create a TodoMVC template"}
+            ]
+        ],
+        li![
+            div![
+                C!["view"],
+                input![C!["toggle"], attrs! {At::Type => "checkbox"}],
+                label!["Buy a unicorn"],
+                button![C!["destroy"]],
+            ],
+            input![C!["edit"], attrs! {At::Value => "Rule the web"}]
+        ]
+    ]
+}
+
+// ------ footer ------
+
+fn view_footer() -> Node<Msg> {
+    footer![
+        C!["footer"],
+        // This should be `0 items left` by default
+        span![C!["todo-count"], strong!["0"], " item left",],
+        view_filters(),
+        // Hidden if no completed items are left ↓
+        button![C!["clear-completed"], "Clear completed"]
+    ]
+}
+
+fn view_filters() -> Node<Msg> {
+    ul![
+        C!["filters"],
+        li![a![C!["selected"], attrs! {At::Href => "#/"}, "All",],],
+        li![a![attrs! {At::Href => "#/active"}, "Active",],],
+        li![a![attrs! {At::Href => "#/completed"}, "Completed",],],
+    ]
 }
 
 // ------ ------
@@ -155,6 +245,11 @@ fn view(model: &Model) -> Node<Msg> {
 // ------ ------
 #[wasm_bindgen(start)]
 pub fn start() {
+    console_error_panic_hook::set_once();
+    let root_element = document()
+        .get_elements_by_class_name("todoapp")
+        .item(0)
+        .expect("element with the class `todoapp`");
     // Mount the `app` to the element with the `id` "app".
-    App::start("app", init, update, view);
+    App::start(root_element, init, update, view);
 }
