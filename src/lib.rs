7e64cc4030cb8104d84a4c8d665e352d4988fa7f
kento tajiri <k.denjiry@gmail.com>
2020-09-11 00:10:03 +0900

    finish to setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
-// (Lines like the one below ignore selected Clippy rules
-//  - it's useful when you want to check your code with `cargo make verify`
-// but some rules are too "annoying" or are not applicable for your case.)
 #![allow(clippy::wildcard_imports)]
+// TODO: Remove
+#![allow(dead_code, unused_variables)]
 use seed::{prelude::*, *};
 use std::collections::BTreeMap;
 use ulid::Ulid;
@@ -9,10 +8,14 @@ use ulid::Ulid;
 // ------ ------
 //     Init
 // ------ ------
-
-// `init` describes what should happen when your app started.
 fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
-    Model::default()
+    Model {
+        todos: BTreeMap::new(),
+        new_todo_title: String::new(),
+        selected_todo: None,
+        filter: Filter::All,
+        base_url: Url::new(),
+    }
 }
 
 struct Model {
@@ -21,6 +24,39 @@ struct Model {
     selected_todo: Option<SelectedTodo>,
     filter: Filter,
     base_url: Url,
+}
+
+impl Model {
+    fn add_mock_data(mut self) -> Self {
+        let (id_a, id_b) = (Ulid::new(), Ulid::new());
+
+        self.todos.insert(
+            id_a,
+            Todo {
+                id: id_a,
+                title: "I'm todo A".to_owned(),
+                completed: false,
+            },
+        );
+
+        self.todos.insert(
+            id_b,
+            Todo {
+                id: id_b,
+                title: "I'm todo B".to_owned(),
+                completed: true,
+            },
+        );
+
+        self.new_todo_title = "I'm a new todo title".to_owned();
+
+        self.selected_todo = Some(SelectedTodo {
+            id: id_b,
+            title: "I'm better todo B".to_owned(),
+            input_element: ElRef::new(),
+        });
+        self
+    }
 }
 
 struct Todo {
@@ -44,7 +80,6 @@ enum Filter {
 // ------ ------
 //    Update
 // ------ ------
-
 enum Msg {
     UrlChanged(subs::UrlChanged),
     NewTodoTitleChanged(String),
@@ -63,37 +98,61 @@ enum Msg {
     SelectedTodoTitleChanged(String),
     SaveSelectedTodo,
 }
+
 // ------ ------
 //    Update
 // ------ ------
-
-// `update` describes how to handle each `Msg`.
 fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
     match msg {
-        Msg::Increment => *model += 1,
+        Msg::UrlChanged(subs::UrlChanged(url)) => {
+            log!("UrlChanged", url);
+        }
+        Msg::NewTodoTitleChanged(title) => {
+            log!("NewTodoTitleChanged", title);
+        }
+
+        // ------ Basic Todo operations ------
+        Msg::CreateTodo => {
+            log!("CreateTodo");
+        }
+        Msg::ToggleTodo(id) => {
+            log!("ToggleTodo");
+        }
+        Msg::RemoveTodo(id) => {
+            log!("RemoveTodo");
+        }
+
+        // ------ Bulk operations ------
+        Msg::CheckOrUncheckAll => {
+            log!("CheckOrUncheckAll");
+        }
+        Msg::ClearCompleted => {
+            log!("ClearCompleted");
+        }
+
+        // ------ Selection ------
+        Msg::SelectTodo(opt_id) => {
+            log!("SelectTodo", opt_id);
+        }
+        Msg::SelectedTodoTitleChanged(title) => {
+            log!("SelectedTodoTitleChanged", title);
+        }
+        Msg::SaveSelectedTodo => {
+            log!("SaveSelectedTodo");
+        }
     }
 }
 
 // ------ ------
 //     View
 // ------ ------
-
-// (Remove the line below once your `Model` become more complex.)
-#[allow(clippy::trivially_copy_pass_by_ref)]
-// `view` describes what to display.
 fn view(model: &Model) -> Node<Msg> {
-    div![
-        "This is a counter: ",
-        C!["counter"],
-        button![model, ev(Ev::Click, |_| Msg::Increment),],
-    ]
+    div!["I'm a placeholder"]
 }
 
 // ------ ------
 //     Start
 // ------ ------
-
-// (This function is invoked by `init` function in `index.html`.)
 #[wasm_bindgen(start)]
 pub fn start() {
     // Mount the `app` to the element with the `id` "app".
